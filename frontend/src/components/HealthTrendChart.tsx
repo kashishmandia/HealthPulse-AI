2f10af1d80089569c98feedf6601e7fc8286bd84
kashishmandia <auraofkash@gmail.com>
2025-12-12 06:34:15 +0530

    Remove unused React imports from HealthTrendChart

--- frontend/src/components/HealthTrendChart.tsx
+++ frontend/src/components/HealthTrendChart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+
 import { apiClient } from '../services/api';
 import { 
   AreaChart, Area, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid 
